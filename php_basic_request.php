1dc045dbadee070dd7b34aab055ac8057958edc7
caleb-imaige <caleb@imaige.com>
2024-02-19 20:52:23 -0700

    cleaned up documentation

--- php_basic_request.php
+++ php_basic_request.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php'; // Make sure to include the autoload file
+require 'vendor/autoload.php';
 require './proto_models/internal_api_template_service/InternalApiTemplateServiceClient.php';
 require './proto_models/internal_api_template_service/TemplateRequest.php';
 require './proto_models/internal_api_template_service/TemplateReply.php';
@@ -27,19 +27,19 @@ $client = new InternalApiTemplateServiceClient($hostname, $opts, $channel);
 $imageData = file_get_contents('./test_image.jpg');
 $imageBase64 = base64_encode($imageData);
 
-// Create a TemplateRequest
+// Create a TemplateRequest - used for testing purposes
 // $request = new TemplateRequest();
 // $request->setName("calebtest");
 
 // Create a request message
 $request = new ImageRequest();
 $request->setB64Image($imageBase64);
-//$request->setText($text);
+//$request->setText($text); // make sure to add any new fields to the .proto file and re-run protoc
 
-// Call the RPC method on the server
+// Call RPC request method on the server
 try {
     $call = $client->InternalApiTemplateImageRequest($request);
-    // Iterate through the stream of TemplateReply messages
+    // Iterate through the stream of responses
     foreach ($call->responses() as $response) {
         // echo "Response from server: " . $response->getMessage() . PHP_EOL;
         // echo "Response from server: " . $response->getb64image() . PHP_EOL;
